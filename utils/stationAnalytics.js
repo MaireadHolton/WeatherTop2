b19a7510bcdde8cfd1f30e711a9555422823c931
Glitch (spiny-broadleaf-saltasaurus) <none>
2022-09-03 10:35:33 +0000

    💰👀 Checkpoint ./utils/stationAnalytics.js:47177237/1414

--- utils/stationAnalytics.js
+++ utils/stationAnalytics.js
@@ -39,28 +39,60 @@ getReadings(station) {
     return readings;
   },
       
-  let lastWeather = null;
-      if (station.readings.size() > 0) {
-            for (Reading reading : readings) {
-                if (reading.code == 100) {
-                    lastWeather = "Clear";
-                } else if (reading.code == 200) {
-                    lastWeather = "partial clouds";
-                } else if (reading.code == 300) {
-                    lastWeather = "Cloudy";
-                } else if (reading.code == 400) {
-                    lastWeather = "Light Showers";
-                } else if (reading.code == 500) {
-                    lastWeather = "Heavy Showers";
-                } else if (reading.code == 600) {
-                    lastWeather = "Rain";
-                } else if (reading.code == 700) {
-                    lastWeather = "Snow";
-                } else if (reading.code == 800) {
-                    lastWeather = "Thunder";
-                }
+  getWeather(station){
+    let lastWeather = null;
+    if (station.readings.length > 0) {
+        for (Reading reading : readings) {
+            if (reading.code == 100) {
+              lastWeather = "Clear";
+              } else if (reading.code == 200) {
+              lastWeather = "partial clouds";
+              } else if (reading.code == 300) {
+              lastWeather = "Cloudy";
+              } else if (reading.code == 400) {
+              lastWeather = "Light Showers";
+              } else if (reading.code == 500) {
+              lastWeather = "Heavy Showers";
+              } else if (reading.code == 600) {
+              lastWeather = "Rain";
+              } else if (reading.code == 700) {
+              lastWeather = "Snow";
+              } else if (reading.code == 800) {
+              lastWeather = "Thunder";
+              }
             }
         }
         return lastWeather;
+  },
+    
+    getIcon(station){
+      let code = null;
+      HashMap<Integer, String> weatherIcons = null;
+        if (readings.size() > 0) {
+            code = readings.size();
+            for (Reading reading : readings) {
+                code = reading.code;
+                {
+                    weatherIcons = new HashMap<Integer, String>();
+                    weatherIcons.put(0, "big red exclamation circle icon");
+                    weatherIcons.put(100, "big orange sun icon");
+                    weatherIcons.put(200, "big yellow cloud sun icon");
+                    weatherIcons.put(300, "big white cloud icon");
+                    weatherIcons.put(400, "big blue cloud sun rain icon");
+                    weatherIcons.put(500, "big grey cloud showers heavy icon");
+                    weatherIcons.put(600, "big grey cloud rain icon");
+                    weatherIcons.put(700, "big blue snowflake icon");
+                    weatherIcons.put(800, "big yellow bolt icon");
+                }
+            }
+            return weatherIcons.get(code);
+        }
+        else {
+            code = 0;
+        }
+        return null;
+    },
+      
+
 
       
