8dc3bb16b4581db13b27811929f4be239a6e3339
Glitch (spiny-broadleaf-saltasaurus) <none>
2022-09-10 13:12:44 +0000

    📮😋 Checkpoint ./utils/stationAnalytics.js:47177237/121

--- utils/stationAnalytics.js
+++ utils/stationAnalytics.js
@@ -233,27 +233,27 @@ const stationAnalytics = {
       return "East";
     } else if (windDir > 78.75 && windDir <= 101.25) {
       return "East South East";
-    } else if (windDir > 101.25 && windDirection <= 123.75) {
+    } else if (windDir > 101.25 && windDir <= 123.75) {
       return "East South East";
-    } else if (windDir > 123.75 && windDirection <= 146.25) {
+    } else if (windDir > 123.75 && windDir <= 146.25) {
       return "South East";
-    } else if (windDirection > 146.25 && windDirection <= 168.75) {
+    } else if (windDir > 146.25 && windDir <= 168.75) {
       return "South South East";
-    } else if (windDirection > 168.75 && windDirection <= 191.25) {
+    } else if (windDir > 168.75 && windDir <= 191.25) {
       return "South";
-    } else if (windDirection > 191.25 && windDirection <= 213.75) {
+    } else if (windDir > 191.25 && windDir <= 213.75) {
       return "South South West";
-    } else if (windDirection > 213.75 && windDirection <= 236.25) {
+    } else if (windDir > 213.75 && windDir <= 236.25) {
       return "South West";
-    } else if (windDirection > 236.25 && windDirection <= 258.75) {
+    } else if (windDir > 236.25 && windDir<= 258.75) {
       return "West South West";
-    } else if (windDirection > 258.75 && windDirection <= 281.25) {
+    } else if (windDir > 258.75 && windDir <= 281.25) {
       return "West";
-    } else if (windDirection > 281.25 && windDirection <= 303.75) {
+    } else if (windDir > 281.25 && windDir <= 303.75) {
       return "West North West";
-    } else if (windDirection > 303.75 && windDirection <= 326.25) {
+    } else if (windDir > 303.75 && windDir <= 326.25) {
       return "North West";
-    } else if (windDirection > 326.25 && windDirection <= 348.75) {
+    } else if (windDir > 326.25 && windDir <= 348.75) {
       return "North North West";
     } else {
       return "North";
